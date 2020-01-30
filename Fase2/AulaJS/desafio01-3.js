39af7a3f537809b0472ca5a21ed089c0294c8138
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-01-29 21:58:50 -0300

    desafio 3 finalmente terminado

--- Fase2/AulaJS/desafio01-3.js
+++ Fase2/AulaJS/desafio01-3.js
@@ -1,6 +1,8 @@
 // DESAFIO 01-3
 //Usuarios e tecnologias
 // criar usuarios com array e em cada usuario os atributos "nome" "tecnologias" em um novo array
+
+/*
 const usuarios = [
     {
         nome: 'Matheus',
@@ -33,6 +35,7 @@ function verificarSeUsaCSS(usuario){
     
             
         }*/
+        /*
         return usuario.tecnologias.includes("CSS")
         
     }   
@@ -43,6 +46,54 @@ function verificarSeUsaCSS(usuario){
           console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`)
         }
       }
+*/
+      //   SOMA DE DESPESAS E RECEITAS
+      const usuarios = [
+        {
+          nome: 'Salvio',
+          receitas: [115.3, 48.7, 98.3, 14.5],
+          despesas: [85.3, 13.5, 19.9]
+        },
+        {
+          nome: 'Marcio',
+          receitas: [24.6, 214.3, 45.3],
+          despesas: [185.3, 12.1, 120.0]
+        },
+        {
+          nome: 'Lucia',
+          receitas: [9.8, 120.3, 340.2, 45.3],
+          despesas: [450.2, 29.9]
+        }
+      ]
 
+function somaNumeros(numeros){
+    let soma = 0
+    for (let i = 0; i < numeros.length; i++) {
+        const numero = numeros[i]
+        soma = soma + numero
+        
+        
+    }
+    return soma
     
+}   
+
+
+function calculaSaldo(receitas, despesas) {
+            const totalReceita = somaNumeros(receitas)
+            const totalDespesa = somaNumeros(despesas)
+            const saldo = totalReceita - totalDespesa
+            return saldo
+}
+for(let i = 0; i < usuarios.length; i++){
+  const usuario = usuarios[i]
+  const saldoFinal = calculaSaldo(usuario.receitas, usuario.despesas)
+
+  if(saldoFinal < 0){
+    console.log(`${usuario.nome} possui saldo POSITIVO de ${saldoFinal.toFixed(2)}`)
+  }else {
+    console.log(`${usuario.nome} possui saldo NEGATIVO de ${saldoFinal.toFixed(2)}`)
+  }
+}
+
 
