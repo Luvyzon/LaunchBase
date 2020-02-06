6559facdc0631e8a3abbe8552754a78f74b57628
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-02-05 22:09:33 -0300

    back end iniciado

---
+++ Fase2/Back-END/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id:"qb8yXWFRROg",
+        title:"Comecei a receber propostas de emprego MUITO boas por um simples motivo",
+        duration: "10min",
+        price: "free"
+    },
+    {
+        id:"qb8yXWFRROg",
+        title:"Comecei a receber propostas de emprego MUITO boas por um simples motivo",
+        duration: "10min",
+        price: "free"
+    },
+    {
+        id:"qb8yXWFRROg",
+        title:"Comecei a receber propostas de emprego MUITO boas por um simples motivo",
+        duration: "10min",
+        price: "free"
+    },
+    {
+        id:"qb8yXWFRROg",
+        title:"Comecei a receber propostas de emprego MUITO boas por um simples motivo",
+        duration: "10min",
+        price: "free"
+    },
+    {
+        id:"qb8yXWFRROg",
+        title:"Comecei a receber propostas de emprego MUITO boas por um simples motivo",
+        duration: "10min",
+        price: "free"
+    },
+    {
+        id:"qb8yXWFRROg",
+        title:"Comecei a receber propostas de emprego MUITO boas por um simples motivo",
+        duration: "10min",
+        price: "free"
+    }
+]
---
+++ Fase2/Back-END/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false
+})
+
+
+server.get("/", function(req, res){
+   const about = {
+       avatar_url:"foto perfil.jpg",
+       name: "Matheus Luvison",
+       role: "Estudante - Bootcamp",
+       description: "Aprendendo a programar para entrar na carreira de programador e ser feliz na vida.",
+       links: [
+           {name: "GitHub", url:"http://www.github.com/Luvytek"},
+           {name: "Linkedin", url:"https://www.linkedin.com/in/matheusluvison"},
+           {name: "Facebook", url:"hhttps://www.facebook.com/MatheusALuvisonk"}
+       ]
+   }
+   
+   
+    return res.render("about", {about})
+
+})
+server.get("/classes", function(req, res){
+    return res.render("classes", {items: videos})
+
+})
+
+server.listen(5000, function(){
+    console.log("server is running!")
+})
