b43278e638c3877f3b00515bd06c2bdb932b89be
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-02-06 13:10:04 -0300

    desafio 03-1 concluido

---
+++ Fase2/Back-END/desafio03-1/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+
+server.use(express.static('public'))
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false
+})
+server.get("/", function(req, res){
+
+    return res.render("conteudo")
+})
+server.get("/sobre", function(req, res){
+    return res.render("sobre")
+})
+server.get("/conteudo", function(req, res){
+    return res.render("conteudo")
+})
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+server.listen(5000, function(){
+    console.log("server is running!")
+})
