fb231b851ce1349d513e9cc9020ba3ff3dea1f84
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-02-12 12:45:39 -0300

    refat foodfy iniciado

--- Fase2/Foodfy/server.js
+++ Fase2/Foodfy/server.js
@@ -4,7 +4,6 @@ const nunjucks = require('nunjucks')
 const recipes = require("./data")
 
 server.use(express.static('public'))
-
 server.set("view engine", "njk")
 
 
@@ -12,4 +11,14 @@ nunjucks.configure("views", {
     express: server,
     autoescape: false,
     noCache: true
+})
+
+server.get("/", function(req, res){
+    return res.render("inicio")
+})
+
+
+
+server.listen(5000, function(){
+    console.log("server is running!")
 })
