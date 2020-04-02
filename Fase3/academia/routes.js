496ec4b05ee1fefae9fd66ae376e79584aa0e7ca
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-04-02 20:52:02 -0300

    criando academia

---
+++ Fase3/academia/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', function(req, res){
+    return res.redirect('/instructors')
+})
+
+routes.get('/instructors', function(req, res){
+
+
+    return res.render('instructors')
+})
+
+routes.get('/members', function(req, res){
+
+    return res.render('/members')
+})
+
+module.exports =  routes
