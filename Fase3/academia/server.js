9510e34d652dc4a61118803c1585cc6825dd3145
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-04-02 19:34:09 -0300

    inciando modulo 3 finalmente

---
+++ Fase3/academia/server.js
@@ -0,0 +1,49 @@
+const express = require ('express');
+const nunjucks = require ('nunjucks');
+const videos = require ('./data');
+
+const server = express();
+
+server.use (express.static ('public'));
+server.set ('view engine', 'njk');
+
+nunjucks.configure ('views', {
+    express: server,
+    autoescape: false,
+    noCache: true,
+});
+
+server.get ('/', function (req, res) {
+   const about = {
+       avatar_url:'foto perfil.jpg',
+       name: 'Matheus Luvison',
+       role: 'Estudante - Bootcamp',
+       description: 'Aprendendo a programar para entrar na carreira de programador e ser feliz na vida.',
+       links: [
+           { name: 'GitHub', url:'http://www.github.com/Luvytek' },
+           { name: 'Linkedin', url:'https://www.linkedin.com/in/matheusluvison' },
+           { name: 'Facebook', url:'hhttps://www.facebook.com/MatheusALuvisonk' },
+       ],
+   };
+ 
+    return res.render ('about', { about });
+});
+
+server.get ('/classes', function (req, res) {
+    return res.render ('classes', { items: videos });
+});
+ 
+server.get ('/video', function (req, res) {
+    const id = req.query.id;
+    const video = videos.find (function (video) {
+        if (video.id == id) true
+    })
+    if (!video) {
+        return res.send ('video not found')
+    }
+    return res.render ('video', { item: video });
+});
+
+server.listen (5000, function () {
+    console.log('server is running!');
+});
