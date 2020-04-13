ec9047625249e97ef241393e5d518de0c370a5bc
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-04-13 20:09:40 -0300

    MODULO 3 INICIADO PARTINDO DESAFIO 4-3

--- Fase3/academia/routes.js
+++ Fase3/academia/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require("./instructors")
 
 routes.get('/', function(req, res){
     return res.redirect('/instructors')
@@ -14,9 +15,7 @@ routes.get('/instructors/create', function(req, res){
     
     return res.render('instructors/create')
 })
-routes.post('/instructors', function(req, res){
-    return res.send('received')
-})
+routes.post('/instructors', instructors.post)
 routes.get('/members', function(req, res){
 
     return res.render('members')
--- Fase3/academia/server.js
+++ Fase3/academia/server.js
@@ -4,6 +4,7 @@ const routes = require ('./routes');
 
 const server = express();
 
+server.use(express.urlencoded({extended: true}))
 server.use ('/public',express.static ('public'));
 server.set ('view engine', 'njk');
 server.use (routes)
