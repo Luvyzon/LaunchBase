113ec9406f13d94f4104e98a77e62333ca2d709e
Matheus Luvison <Matheus@MacBook-Matheus.local>
2020-04-29 21:49:16 -0300

    pagina show terminada

--- Foodfy/public/scripts.js
+++ Foodfy/public/scripts.js
@@ -6,7 +6,6 @@ const content1 = document.querySelector('.recipelist1')
 const content2 = document.querySelector('.recipelist2')
 const content3 = document.querySelector('.recipelist3')
 
-
 for (let card of cards) {
   card.addEventListener('click', function () {
     const receitaId = card.getAttribute('id')
@@ -22,47 +21,41 @@ for (let recipe of recipes) {
 }
 */
 
-hideshow1.addEventListener('click', function() {
-  if(hideshow1.textContent == "MOSTRAR"){
-  content1.classList.remove('recipelist1')
-  content1.classList.add('recipelist1show')
-  hideshow1.innerHTML = "ESCONDER"
+hideshow1.addEventListener('click', function () {
+  if (hideshow1.textContent == 'MOSTRAR') {
+    content1.classList.remove('recipelist1')
+    content1.classList.add('recipelist1show')
+    hideshow1.innerHTML = 'ESCONDER'
+  } else if (hideshow1.textContent == 'ESCONDER') {
+    content1.classList.remove('recipelist1show')
+    content1.classList.add('recipelist1')
+    hideshow1.innerHTML = 'MOSTRAR'
   }
-  else if(hideshow1.textContent == "ESCONDER"){
-    
-      content1.classList.remove('recipelist1show')
-      content1.classList.add('recipelist1')
-      hideshow1.innerHTML = "MOSTRAR"
-    }
-    
 })
 
 hideshow2.addEventListener('click', function() {
-  if(hideshow2.textContent == "MOSTRAR"){
-  content2.classList.remove('recipelist2')
-  content2.classList.add('recipelist2show')
-  hideshow2.innerHTML = "ESCONDER"
+  if (hideshow2.textContent == 'MOSTRAR') {
+    content2.classList.remove('recipelist2')
+    content2.classList.add('recipelist2show')
+    hideshow2.innerHTML = 'ESCONDER'
+  } else if (hideshow2.textContent == 'ESCONDER') {
+    content2.classList.remove('recipelist2show')
+    content2.classList.add('recipelist2')
+    hideshow2.innerHTML = 'MOSTRAR'
   }
-  else if(hideshow2.textContent == "ESCONDER"){
-    
-      content2.classList.remove('recipelist2show')
-      content2.classList.add('recipelist2')
-      hideshow2.innerHTML = "MOSTRAR"
-    }
-    
 })
 
 hideshow3.addEventListener('click', function() {
-  if(hideshow3.textContent == "MOSTRAR"){
+  if(hideshow3.textContent == 'MOSTRAR'){
   content3.classList.remove('recipelist3')
   content3.classList.add('recipelist3show')
-  hideshow3.innerHTML = "ESCONDER"
+  hideshow3.innerHTML = 'ESCONDER'
   }
-  else if(hideshow3.textContent == "ESCONDER"){
+  else if(hideshow3.textContent == 'ESCONDER'){
     
       content3.classList.remove('recipelist3show')
       content3.classList.add('recipelist3')
-      hideshow3.innerHTML = "MOSTRAR"
+      hideshow3.innerHTML = 'MOSTRAR'
     }
     
 })
