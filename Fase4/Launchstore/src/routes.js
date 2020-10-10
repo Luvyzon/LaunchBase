07dc23dcd69323caef0836b910e1bf2f1f8df795
Luvytek <matheusluvison18@outlook.com>
2020-10-09 21:21:58 -0300

    making products create launchstore

--- Fase4/Launchstore/src/routes.js
+++ Fase4/Launchstore/src/routes.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const ProductController = require('./app/controllers/ProductController')
 const routes = express.Router()
 
 routes.get('/', function(req, res){
     return res.render("layout.njk")
+})
+
+routes.get('/products/create', ProductController.create)
+routes.get('/ads/create', function(req, res){
+  return res.redirect("/products/create")
 })
 
 module.exports =  routes
