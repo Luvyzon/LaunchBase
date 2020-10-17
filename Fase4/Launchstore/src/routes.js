13318147de0fda3b0426e1c1c3f2fc1f5bbd3d4b
Luvytek <matheusluvison18@outlook.com>
2020-10-17 20:42:42 -0300

    starting challenge 6-1

--- Fase4/Launchstore/src/routes.js
+++ Fase4/Launchstore/src/routes.js
@@ -7,6 +7,13 @@ routes.get('/', function(req, res){
 })
 
 routes.get('/products/create', ProductController.create)
+routes.get('/products/:id/edit', ProductController.edit)
+
+routes.post('/products', ProductController.post)
+routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
+
+//Alias
 routes.get('/ads/create', function(req, res){
   return res.redirect("/products/create")
 })
