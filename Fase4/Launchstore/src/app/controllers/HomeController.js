52e927676c7dff7a6be06cd2c44f680499629652
Luvytek <matheusluvison18@outlook.com>
2021-02-02 13:55:11 -0300

    launchstore search finished

--- Fase4/Launchstore/src/app/controllers/HomeController.js
+++ Fase4/Launchstore/src/app/controllers/HomeController.js
@@ -5,26 +5,31 @@ const File = require('../models/File')
 
 module.exports = {
   async index(req, res) {
-    let results = await Product.all()
-    const products = results.rows
+    try {
+      let results = await Product.all()
+      const products = results.rows
 
-    if (!products) return res.send("Products not found!")
+      if (!products) return res.send("Products not found!")
 
-    async function getImage(productId) {
-      let results = await Product.files(productId)
-      const files = results.rows.map(file =>  `${req.protocol}://${req.headers.host}/${file.path}`
-      )
-      return files[0]
-    }
-    const productsPromise = products.map(async product => {
-      product.img = await getImage(product.id)
-      product.old_price = formatPrice(product.old_price)
-      product.price = formatPrice(product.price)
-      return product
-    }).filter((product, index) => index > 2 ? false : true)
-   
-    const lastAdded = await Promise.all(productsPromise)
+      async function getImage(productId) {
+        let results = await Product.files(productId)
+        const files = results.rows.map(file =>  `${req.protocol}://${req.headers.host}/${file.path}`
+        )
+        return files[0]
+      }
+      const productsPromise = products.map(async product => {
+        product.img = await getImage(product.id)
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+        return product
+      }).filter((product, index) => index > 2 ? false : true)
     
-    return res.render("home/index", {products: lastAdded})
+      const lastAdded = await Promise.all(productsPromise)
+      
+      return res.render("home/index", {products: lastAdded})
   }
+  catch(err) {
+    console.log(err)
+  }
+}
 }
