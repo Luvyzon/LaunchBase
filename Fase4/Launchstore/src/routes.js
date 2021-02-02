e2bd0f406a1c248990445aad9642f7395aa20065
Luvytek <matheusluvison18@outlook.com>
2021-02-02 11:35:28 -0300

    making a sql parameters os search page

--- Fase4/Launchstore/src/routes.js
+++ Fase4/Launchstore/src/routes.js
@@ -3,12 +3,19 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
+//Home
 routes.get('/', HomeController.index)
 
+
+//Search
+routes.get('/products/search', SearchController.index)
+
+// Products
 routes.get('/products/create', ProductController.create)
-routes.get('/product/:id', ProductController.show)
-routes.get('/product/:id/edit', ProductController.edit)
+routes.get('/products/:id', ProductController.show)
+routes.get('/products/:id/edit', ProductController.edit)
 
 routes.post('/products', multer.array("photos", 6), ProductController.post)
 routes.put('/products', multer.array("photos", 6), ProductController.put)
@@ -18,5 +25,6 @@ routes.delete('/products', ProductController.delete)
 routes.get('/ads/create', function(req, res){
   return res.redirect("/products/create")
 })
+
 
 module.exports =  routes
