da3a450f60ab4dab9fb9624813a3a85c08042888
Luvytek <matheusluvison18@outlook.com>
2021-02-08 12:27:32 -0300

    initializing users control

---
+++ Fase4/Admin/src/server.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const nunjucks = require ('nunjucks');
+const routes = require ('./routes');
+const methodOverride = require('method-override')
+const server = express();
+
+server.use(express.urlencoded({extended: true}))
+server.use ('/public',express.static ('public'));
+server.set ('view engine', 'njk');
+server.use (methodOverride('_method'))
+server.use (routes)
+
+nunjucks.configure ('src/app/views', {
+    express: server,
+    autoescape: false,
+    noCache: true,
+});
+
+
+
+server.listen (5000, function () {
+    console.log('server is running!');
+});
