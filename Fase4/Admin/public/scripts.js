02cc41f869a45ce224381c8c794d45c1a21e737c
Luvytek <matheusluvison18@outlook.com>
2021-02-09 15:53:00 -0300

    making user controller

--- Fase4/Admin/public/scripts.js
+++ Fase4/Admin/public/scripts.js
@@ -10,6 +10,68 @@ const Mask = {
       style: 'currency',
       currency:'BRL'
     }).format(value/100)
+  },
+  cpfCnpj(value){
+    value = value.replace(/\D/g, "")
+
+    if (value.length > 14){
+      value = value.slice(0, -1)
+    }
+
+    if (value.length > 11){
+
+      value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d)/, "$1.$2.$3/$4-$5")
+
+    } else {
+      value = value.replace(/(\d{3})(\d{3})(\d{3})(\d)/, "$1.$2.$3-$4")
+    }
+    return value
+  },
+  cep(value){
+    value = value.replace(/\D/g, "")
+    if (value.length > 8){
+      value = value.slice(0, -1)
+    }
+
+    value = value.replace(/(\d{5})(\d)/, "$1-$2")
+
+    return value
+  }
+}
+
+const Validate = {
+  apply(input, func) {
+    Validate.clearErrors(input)
+
+    let results = Validate[func](input.value)
+    input.value = results.value
+
+    if (results.error)
+      Validate.displayError(input, results.error)
+  },
+  displayError(input, error) {
+    const div = document.createElement('div')
+    div.classList.add('error')
+    div.innerHTML = error 
+    input.parentNode.appendChild(div)
+
+    input.focus()
+  },
+  clearErrors(input){
+    const errorDiv = input.parentNode.querySelector(".error")
+    if (errorDiv)
+      errorDiv.remove()
+  },
+  isEmail(value){
+    let error = null
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if(!value.match(mailFormat))
+      error = "Email Invalido"
+    return {
+      error,
+      value
+    }
   }
 }
 
---
+++ Fase4/Admin/src/routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const routes = express.Router()
+
+const HomeController = require('../app/controllers/HomeController')
+
+const users = require('./users')
+const products = require('./products')
+
+
+
+//Home
+routes.get('/', HomeController.index)
+
+routes.use('/products', products)
+routes.use('/users', users)
+
+//Alias
+routes.get('/ads/create', function(req, res){
+  return res.redirect("/products/create")
+})
+routes.get('/accounts', function(req, res){
+  return res.redirect("/users/register")
+})
+
+module.exports =  routes
