32540e15acd0626f1e8266fa8825c3e9fc53442e
Luvytek <matheusluvison18@outlook.com>
2021-02-22 21:36:37 -0300

    registerForm

--- Foodfy/src/routes/users.js
+++ Foodfy/src/routes/users.js
@@ -6,24 +6,29 @@ const ProfileController = require('../app/controllers/ProfileController')
 const UserController = require('../app/controllers/UserController')
 
 // LOGIN / LOGOUT
+
 //routes.get('/login', SessionController.loginForm)
 //routes.post('/login', SessionController.login)
 //routes.post('/logout', SessionController.logout)
 
 //reset password
+
 //routes.get('/forgot-password', SessionController.forgotForm)
 //routes.get('/password-reset', SessionController.resetForm)
 //routes.post('/forgot-password', SessionController.forgot)
 //routes.post('/password-reset', SessionController.reset)
 
 // Rotas de perfil de um usuário logado
-//routes.get('/admin/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
-//routes.put('/admin/profile', ProfileController.put)// Editar o usuário logado
+
+//routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
+//routes.put('/profile', ProfileController.put)// Editar o usuário logado
 
 // Rotas que o administrador irá acessar para gerenciar usuários
-//routes.get('/admin/users', UserController.list) //Mostrar a lista de usuários cadastrados
-//routes.post('/admin/users', UserController.post) //Cadastrar um usuário
-//routes.put('/admin/users', UserController.put) // Editar um usuário
-//routes.delete('/admin/users', UserController.delete) // Deletar um usuário
+
+routes.get('/register', UserController.registerForm)
+//routes.get('/', UserController.list) //Mostrar a lista de usuários cadastrados
+//routes.post('/', UserController.post) //Cadastrar um usuário
+//routes.put('/', UserController.put) // Editar um usuário
+//routes.delete('/', UserController.delete) // Deletar um usuário
 
 module.exports = routes
